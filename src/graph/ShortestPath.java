607a029bcb557c2f9a6bb976e0923366274f4406
khidr <akhidr@bitbucket.org>
2019-11-29 18:12:05 +0200

    get shortest path using graph

--- src/graph/ShortestPath.java
+++ src/graph/ShortestPath.java
@@ -13,28 +13,6 @@ public class ShortestPath {
     this.graph = graph;
   }
 
-  public static void main(String[] args) {
-    Graph graph = new Graph(9);
-    graph.addEdge(1, 2);
-    graph.addEdge(1, 3);
-    graph.addEdge(3, 0);
-    graph.addEdge(3, 4);
-    graph.addEdge(3, 6);
-    graph.addEdge(0, 4);
-    graph.addEdge(0, 5);
-    graph.addEdge(4, 5);
-    graph.addEdge(7, 8);
-
-    ShortestPath shortestPath = new ShortestPath(graph);
-
-    int[] distances = shortestPath.getDistances(1);
-
-    for (int i = 0; i < 9; i++) {
-      System.out.println(i + ": " + distances[i]);
-    }
-
-  }
-
   public int[] getDistances(int startId) {
 
     LinkedList<Integer> queue = new LinkedList<>();
@@ -57,4 +35,28 @@ public class ShortestPath {
 
     return distances;
   }
+
+  public static void main(String[] args) {
+    Graph graph = new Graph(9);
+    graph.addEdge(1, 2);
+    graph.addEdge(1, 3);
+    graph.addEdge(3, 0);
+    graph.addEdge(3, 4);
+    graph.addEdge(3, 6);
+    graph.addEdge(0, 4);
+    graph.addEdge(0, 5);
+    graph.addEdge(4, 5);
+    graph.addEdge(7, 8);
+
+    ShortestPath shortestPath = new ShortestPath(graph);
+
+    int[] distances = shortestPath.getDistances(1);
+
+    for (int i = 0; i < 9; i++) {
+      System.out.println(i + ": " + distances[i]);
+    }
+
+  }
+
+
 }
