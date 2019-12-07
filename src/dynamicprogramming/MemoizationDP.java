2b22fd4aa8fd57446e2952b68dfa8848a1e3db45
khidr <akhidr@bitbucket.org>
2019-12-07 13:48:05 +0200

    dynamic programming problems

--- src/dynamicprogramming/MemoizationDP.java
+++ src/dynamicprogramming/MemoizationDP.java
@@ -2,21 +2,28 @@ package dynamicprogramming;
 
 public class MemoizationDP {
 
-  public static int getFactorial(int num) {
+  int iteration = 0;
+
+  public int getFactorial(int num) {
     int[] dp = new int[num+1];
     return getFactorialRec(dp, num);
   }
 
-  public static int getFactorialRec(int[] dp, int num) {
+  private int getFactorialRec(int[] dp, int num) {
+    iteration++;
     if (num == 0) return 1;
     dp[num] = getFactorialRec(dp, num - 1) * num;
     return dp[num];
   }
 
   public static void main(String[] args) {
-    for (int i = 5; i >= 1; i--) {
-      System.out.println("Get Factorial of " + i + ": " + getFactorial(i));
-    }
+    MemoizationDP memoizationDP = new MemoizationDP();
+    System.out.println("Fact: " + memoizationDP.getFactorial(8));
+    System.out.println("Iteration: " + memoizationDP.iteration);
+
+//    for (int i = 5; i >= 1; i--) {
+//      System.out.println("Get Factorial of " + i + ": " + getFactorial(i));
+//    }
   }
 
 }
